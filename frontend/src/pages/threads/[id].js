298f0a07855fdc59191b2ef8e161bfd510fcf34f
itomotoki8 <yitengb90@gmail.com>
2023-10-13 14:38:16 +0900

    first-commit

---
+++ frontend/src/pages/threads/[id].js
@@ -0,0 +1,125 @@
+import { useRouter } from "next/router";
+import useSWR from "swr";
+import Layout from "../../components/layout";
+import Link from "next/link";
+import { useState } from "react";
+import { postResponse } from "../../libs/response";
+
+const Thread = () => {
+  const router = useRouter();
+  const { id } = router.query;
+  const { data, mutate } = useSWR(
+    id ? `${process.env.NEXT_PUBLIC_API_URL}/threads/${id}` : null
+  );
+  const thread = data?.data;
+  const [response, setResponse] = useState({
+    name: "",
+    email: "",
+    content: "",
+  });
+
+  const createResponse = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await postResponse(response, id);
+      if (!res.ok) throw new Error("レスの作成に失敗しました");
+      mutate();
+      setResponse({ name: "", email: "", content: "" });
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  const handleChange = (e) => {
+    setResponse({ ...response, [e.target.name]: e.target.value });
+  };
+  return (
+    <Layout title="スレッド詳細" bg="bg-gray-300">
+      <section className="mb-8">
+        <h2 className="p-3 text-xl font-semibold">スレッド詳細</h2>
+        <hr className="mb-2 border-black" />
+        {thread && (
+          <>
+            <div className="mb-4">
+              <h2 className="mb-4 text-xl text-red-600">{thread.title}</h2>
+              <p>
+                {"1 : "}
+                <span className="text-green-700 font-semibold">
+                  {thread.name}
+                </span>
+                {" : "}
+                {thread.created_at}
+              </p>
+              <p className="x-8 whitespace-pre-wrap">{thread.content}</p>
+            </div>
+            {thread.responses &&
+              thread.responses.map((response) => (
+                <div className="mb-4" key={response.id}>
+                  <p>
+                    {response.response_no + 1}
+                    {" : "}
+                    <span className="text-green-700 font-semibold">
+                      {response.content}
+                    </span>
+                    {" : "}
+                    {response.created_at}
+                  </p>
+                  <p className="px-8 whitespace-pre-wrap">{response.content}</p>
+                </div>
+              ))}
+          </>
+        )}
+      </section>
+
+      <hr className="mb-4 border-black" />
+
+      <section>
+        <div className="mb-4">
+          <Link href="/" className="text-sm text-blue-900 underline">
+            掲示板に戻る
+          </Link>
+        </div>
+        <form onSubmit={createResponse}>
+          <div className="space-x-1">
+            <button className="px-1 5 py-0 5 text-sm bg-gray-300 border border-gray-500 rounded">
+              書き込む
+            </button>
+            <label htmlFor="name">名前{" : "}</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              className="p-0.5 text-sm border border-gray-500 rounded"
+              value={response.name}
+              onChange={handleChange}
+            ></input>
+
+            <label htmlFor="email">
+              E-mail<span className="text-xs">(省略可)</span>{" "}
+            </label>
+            <input
+              type="text"
+              id="email"
+              name="email"
+              className="p-0.5 text-sm border border-gray-500 rounded"
+              value={response.email}
+              onChange={handleChange}
+            ></input>
+          </div>
+          <div className="flex">
+            <textarea
+              name="content"
+              id="content"
+              rows="5"
+              className="p-0.5 w-[555px] text-sm border border-gray-500 rounded"
+              value={response.content}
+              onChange={handleChange}
+            ></textarea>
+          </div>
+        </form>
+      </section>
+    </Layout>
+  );
+};
+
+export default Thread;
